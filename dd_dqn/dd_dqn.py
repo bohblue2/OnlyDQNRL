d3762d1898d5ac340ff31ab52bd3516809384500
chageumgang <chagmgang@gmail.com>
2018-04-26 10:45:32 +0900

    update tensorflow rl

---
+++ dd_dqn/dd_dqn.py
@@ -0,0 +1,86 @@
+import numpy as np
+import tensorflow as tf
+from typing import List
+
+
+class DQN:
+
+    def __init__(self, session, input_size, output_size: int, name, mode):
+        self.session = session
+        self.input_size = input_size
+        self.output_size = output_size
+        self.net_name = name
+        self.mode = mode
+
+        self._build_network()
+
+    def _build_network(self, h_size=16, l_rate=0.001):
+        with tf.variable_scope(self.net_name):
+            self._X = tf.placeholder(tf.float32, [None, self.input_size], name="input_x")
+
+            net = tf.layers.dense(self._X, h_size, activation=tf.nn.relu)
+
+            self.advantage = tf.layers.dense(net, self.output_size)
+            self.value = tf.layers.dense(net, 1)
+
+            if self.mode == 'default':
+                self._Qpred = tf.add(self.advantage, self.value)
+            elif self.mode == 'mean':
+                self._Qpred = tf.add(self.advantage, tf.reduce_mean(self.value))
+            elif self.mode == 'max':
+                self._Qpred = tf.add(self.advantage, tf.reduce_max(self.value))
+
+            self._Y = tf.placeholder(tf.float32, shape=[None, self.output_size])
+            self._loss = tf.losses.mean_squared_error(self._Y, self._Qpred)
+
+            optimizer = tf.train.AdamOptimizer(learning_rate=l_rate)
+            self._train = optimizer.minimize(self._loss)
+
+    def predict(self, state):
+        x = np.reshape(state, [-1, self.input_size])
+        return self.session.run(self._Qpred, feed_dict={self._X: x})
+
+    def update(self, x_stack: np.ndarray, y_stack: np.ndarray) -> list:
+        feed = {
+            self._X: x_stack,
+            self._Y: y_stack
+        }
+        return self.session.run([self._loss, self._train], feed)
+
+def replay_train(mainDQN, targetDQN, train_batch):
+    DISCOUNT_RATE = 0.99
+    states = np.vstack([x[0] for x in train_batch])
+    actions = np.array([x[1] for x in train_batch])
+    rewards = np.array([x[2] for x in train_batch])
+    next_states = np.vstack([x[3] for x in train_batch])
+    done = np.array([x[4] for x in train_batch])
+
+    X = states
+
+    Q_value_next_state = mainDQN.predict(next_states)
+    evaluated_action = np.argmax(Q_value_next_state, axis=1)
+    ev_action = []
+    for i in range(64):
+        ev_action.append(targetDQN.predict(next_states)[i][evaluated_action[i]])
+    Q_target = rewards + DISCOUNT_RATE * np.array(ev_action) * ~done
+
+    y = mainDQN.predict(states)
+
+    y[np.arange(len(X)), actions] = Q_target
+
+    # Train our network using target and predicted Q values on each episode
+    return mainDQN.update(X, y)
+
+
+def get_copy_var_ops(*, dest_scope_name, src_scope_name):
+    op_holder = []
+
+    src_vars = tf.get_collection(
+        tf.GraphKeys.TRAINABLE_VARIABLES, scope=src_scope_name)
+    dest_vars = tf.get_collection(
+        tf.GraphKeys.TRAINABLE_VARIABLES, scope=dest_scope_name)
+
+    for src_var, dest_var in zip(src_vars, dest_vars):
+        op_holder.append(dest_var.assign(src_var.value()))
+
+    return op_holder
---
+++ dqn/agent.py
@@ -0,0 +1,72 @@
+import numpy as np
+import tensorflow as tf
+import random
+from collections import deque
+from dqn import DQN, get_copy_var_ops, replay_train
+
+import gym
+
+env = gym.make('CartPole-v0')
+
+# Constants defining our neural network
+INPUT_SIZE = env.observation_space.shape[0]
+OUTPUT_SIZE = env.action_space.n
+
+
+REPLAY_MEMORY = 50000
+BATCH_SIZE = 64
+TARGET_UPDATE_FREQUENCY = 5
+MAX_EPISODES = 5000
+
+def main():
+    replay_buffer = deque(maxlen=REPLAY_MEMORY)
+
+    last_100_game_reward = deque(maxlen=100)
+
+    with tf.Session() as sess:
+        mainDQN = DQN(sess, INPUT_SIZE, OUTPUT_SIZE, name="main")
+        targetDQN = DQN(sess, INPUT_SIZE, OUTPUT_SIZE, name="target")
+        sess.run(tf.global_variables_initializer())
+
+        # initial copy q_net -> target_net
+        copy_ops = get_copy_var_ops(dest_scope_name="target",
+                                    src_scope_name="main")
+        sess.run(copy_ops)
+
+        for episode in range(MAX_EPISODES):
+            e = 1. / ((episode / 10) + 1)
+            done = False
+            step_count = 0
+            state = env.reset()
+
+            while not done:
+                if np.random.rand() < e:
+                    action = env.action_space.sample()
+                else:
+                    # Choose an action by greedily from the Q-network
+                    action = np.argmax(mainDQN.predict(state))
+
+                # Get new state and reward from environment
+                next_state, reward, done, _ = env.step(action)
+
+                if done:  # Penalty
+                    reward = -1
+
+                # Save the experience to our buffer
+                replay_buffer.append((state, action, reward, next_state, done))
+
+                if len(replay_buffer) > BATCH_SIZE:
+                    minibatch = random.sample(replay_buffer, BATCH_SIZE)
+                    loss, _ = replay_train(mainDQN, targetDQN, minibatch)
+
+                if step_count % TARGET_UPDATE_FREQUENCY == 0:
+                    sess.run(copy_ops)
+
+                state = next_state
+                step_count += 1
+
+            print("Episode: {}  steps: {}".format(episode, step_count))
+
+
+if __name__ == "__main__":
+    main()
