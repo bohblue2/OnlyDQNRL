b3118117634b183c0314ce825c86e9afeb52a937
chageumgang <chagmgang@gmail.com>
2018-09-08 15:28:45 +0900

    finish iqn

--- Implicit Quantile Network/IQN.py
+++ Implicit Quantile Network/IQN.py
@@ -5,6 +5,7 @@ from collections import deque
 import gym
 import random
 
+
 class IQN:
     def __init__(self, sess):
         self.action_size = 2
@@ -16,29 +17,26 @@ class IQN:
         self.gamma = 0.99
 
         self.state = tf.placeholder(tf.float32, [None, self.state_size])
-        self.action = tf.placeholder(tf.float32, [None, self.action_size])
+        self.tau = tf.placeholder(tf.float32, [None, self.num_quantiles])
         self.Y = tf.placeholder(tf.float32, [None, self.num_quantiles])
+        self.action = tf.placeholder(tf.float32, [None, self.action_size])
 
-        self.main_network, self.main_params, self.main_quantiles = self._build_net('main')
-        self.target_network, self.target_params, self.target_quantiles = self._build_net('target')
+        self.main_support, self.main_params = self._build_net('main')
+        self.target_support, self.target_params = self._build_net('target')
 
         expand_dim_action = tf.expand_dims(self.action, -1)
-        theta_s_a = tf.reduce_sum(self.main_network * expand_dim_action, axis=1)
-        quant = tf.reshape(self.main_quantiles, [self.batch_size, self.num_quantiles])
-        sorted_quantiles = tf.contrib.framework.sort(quant, direction='ASCENDING')
-        ze = tf.zeros([self.batch_size, 1])
-        shift_sorted_quantiles = tf.concat([ze, sorted_quantiles], 1)[:,:self.num_quantiles]
-        tau = sorted_quantiles - shift_sorted_quantiles
-        inv_tau = 1.0 - tau
+        main_support = tf.reduce_sum(self.main_support * expand_dim_action, axis=1)
+        Huber_loss = tf.losses.huber_loss(self.Y, main_support, reduction=tf.losses.Reduction.NONE)
 
-        Huber_loss = tf.losses.huber_loss(self.Y, theta_s_a, reduction=tf.losses.Reduction.NONE)
+        tau = self.tau
+        inv_tau = 1 - tau
 
-        error_loss = self.Y - theta_s_a
+        error_loss = self.Y - main_support
+
         Loss = tf.where(tf.less(error_loss, 0.0), inv_tau * Huber_loss, tau * Huber_loss)
         Loss = tf.reduce_mean(tf.reduce_sum(Loss, axis=1))
 
-        self.train_op = tf.train.AdamOptimizer(1e-5).minimize(Loss)
-
+        self.train_op = tf.train.AdamOptimizer(1e-4).minimize(Loss)
 
         self.assign_ops = []
         for v_old, v in zip(self.target_params, self.main_params):
@@ -51,65 +49,60 @@ class IQN:
         action_stack = [mini[2] for mini in minibatch]
         reward_stack = [mini[3] for mini in minibatch]
         done_stack = [mini[4] for mini in minibatch]
-
         done_stack = [int(i) for i in done_stack]
 
-        Q_next_state = self.sess.run(self.target_network, feed_dict={self.state: next_state_stack})
+        t = np.random.rand(self.batch_size, self.num_quantiles)
+        Q_next_state = self.sess.run(self.target_support, feed_dict={self.state: next_state_stack, self.tau: t})
         next_action = np.argmax(np.mean(Q_next_state, axis=2), axis=1)
         Q_next_state_next_action = [Q_next_state[i, action, :] for i, action in enumerate(next_action)]
         T_theta = [reward + (1-done)*self.gamma*Q for reward, Q, done in zip(reward_stack, Q_next_state_next_action, done_stack)]
-        for i in T_theta:
-            print(i)
-        self.sess.run(self.train_op, feed_dict={self.state: state_stack, self.action: action_stack, self.Y: T_theta})
+
+        self.sess.run(self.train_op, feed_dict={self.state: state_stack, self.action: action_stack, self.tau:t, self.Y: T_theta})
 
     def _build_net(self, name):
         with tf.variable_scope(name):
-            state_net = tf.layers.dense(inputs=self.state, units=64, activation=tf.nn.relu)
-            state_net = tf.layers.dense(inputs=state_net, units=64, activation=tf.nn.relu)
-            state_net_size = state_net.get_shape().as_list()[-1]
-            state_net_tiled = tf.tile(state_net, [self.num_quantiles, 1])
+            state_tile = tf.tile(self.state, [1, self.num_quantiles])
+            state_reshape = tf.reshape(state_tile, [-1, self.state_size])
+            state_net = tf.layers.dense(inputs=state_reshape, units=128, activation=tf.nn.selu)
 
-            quantile_shape = [self.num_quantiles * self.batch_size, 1]
-            quantiles = tf.random_uniform(quantile_shape, minval=0, maxval=1, dtype=tf.float32) #tau
-            quantile_net = tf.tile(quantiles, [1, self.quantile_embedding_dim])
-            pi = tf.constant(math.pi)
-            quantile_net = tf.cast(tf.range(1, self.quantile_embedding_dim + 1, 1), tf.float32) * pi * quantile_net
-            quantile_net = tf.cos(quantile_net)
-            quantile_net = tf.layers.dense(inputs=quantile_net, units=state_net_size, activation=tf.nn.relu)
+            tau = tf.reshape(self.tau, [-1, 1])
+            pi_mtx = tf.constant(np.expand_dims(np.pi * np.arange(0, 64), axis=0), dtype=tf.float32)
+            cos_tau = tf.cos(tf.matmul(tau, pi_mtx))
+            phi = tf.layers.dense(inputs=cos_tau, units=128, activation=tf.nn.relu)
 
-            net = tf.multiply(state_net_tiled, quantile_net)
+            net = tf.multiply(state_net, phi)
             net = tf.layers.dense(inputs=net, units=512, activation=tf.nn.relu)
             net = tf.layers.dense(inputs=net, units=128, activation=tf.nn.relu)
-            quantile_values = tf.layers.dense(inputs=net, units=self.action_size, activation=None)
-            quantile_values = tf.reshape(quantile_values, [self.batch_size, self.action_size, self.num_quantiles])
+            net = tf.layers.dense(inputs=net, units=self.action_size, activation=None)
+            net = tf.transpose(tf.split(net, self.batch_size, axis=0), perm=[0, 2, 1])
 
             params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=name)
 
-        return quantile_values, params, quantiles
+            return net, params
 
     def choose_action(self, obs):
-        obs = np.tile(obs, [self.batch_size, 1])
-        Q_s_a = self.sess.run(self.main_network, feed_dict={self.state: obs})
-        Q_s_a = np.sum(Q_s_a[0], axis=1)
-        action = np.argmax(Q_s_a)
+        t = np.random.rand(iqn.batch_size, iqn.num_quantiles)
+        obs = np.tile(obs, [iqn.batch_size, 1])
+        Q_s_a = self.sess.run(self.main_support, feed_dict={self.state: obs, self.tau: t})
+        Q_s_a = Q_s_a[0]
+        Q_a = np.sum(Q_s_a, axis=1)
+        action = np.argmax(Q_a)
         return action
-
-
 
 env = gym.make('CartPole-v0')
 sess = tf.Session()
-qrdqn = IQN(sess)
+iqn = IQN(sess)
 sess.run(tf.global_variables_initializer())
-sess.run(qrdqn.assign_ops)
-memory_size = 500000
+
+r = tf.placeholder(tf.float32)  ########
+rr = tf.summary.scalar('reward', r)
+merged = tf.summary.merge_all()  ########
+writer = tf.summary.FileWriter('./board/step', sess.graph)  ########
+
+memory_size = 10000
 memory = deque(maxlen=memory_size)
 
-#r = tf.placeholder(tf.float32)  ########
-#rr = tf.summary.scalar('reward', r)
-#merged = tf.summary.merge_all()  ########
-#writer = tf.summary.FileWriter('/Users/chageumgang/Desktop/OnlyDQNRL/Implicit Quantile Network/board/IQN', sess.graph)  ########
-
-for episode in range(10000):
+for episode in range(3000):
     e = 1. / ((episode / 10) + 1)
     done = False
     state = env.reset()
@@ -119,7 +112,7 @@ for episode in range(10000):
         if np.random.rand() < e:
             action = env.action_space.sample()
         else:
-            action = qrdqn.choose_action(state)
+            action = iqn.choose_action(state)
 
         next_state, reward, done, _ = env.step(action)
 
@@ -127,14 +120,16 @@ for episode in range(10000):
             reward = -1
         else:
             reward = 0
+
+        if len(memory) > 1000:
+            iqn.train(memory)
+            if global_step % 20 == 0:
+                sess.run(iqn.assign_ops)
         action_one_hot = np.zeros(2)
         action_one_hot[action] = 1
         memory.append([state, next_state, action_one_hot, reward, done])
         state = next_state
         if done:
-            if len(memory) > 1000:
-                qrdqn.train(memory)
-                sess.run(qrdqn.assign_ops)
-            #summary = sess.run(merged, feed_dict={r: global_step})
-            #writer.add_summary(summary, episode)
+            summary = sess.run(merged, feed_dict={r: global_step})
+            writer.add_summary(summary, episode)
             print(episode, global_step)
